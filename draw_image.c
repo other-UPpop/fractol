d5713b5c8ef3c3cedc52d09733e7136cbe2b293f
rohta <user@rohtapc.local>
2025-05-06 13:49:07 +0900

    20250506_1346

--- draw_image.c
+++ draw_image.c
@@ -6,7 +6,7 @@
 /*   By: rohta <rohta@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/16 19:28:52 by rohta             #+#    #+#             */
-/*   Updated: 2025/05/06 13:35:22 by rohta            ###   ########.fr       */
+/*   Updated: 2025/05/06 13:48:59 by rohta            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,8 +44,8 @@ int	get_color(int iter, int max_iter)
 	if (iter == max_iter)
 		return (0x000000);
 	r = (iter * 5) % 256;
-	g = (iter * 15) % 256;
-	b = (iter * 25) % 256;
+	g = (iter * 13) % 256;
+	b = (iter * 17) % 256;
 	return (r << 16 | g << 8 | b);
 }
 
